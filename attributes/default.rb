2d80a7660f43b87bcabd475d3c624600931a94fc
Luca Pireddu <pireddu@crs4.it>
2013-06-19 15:32:39 +0200

    beautify attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['hadoop-BAM-release-jar']       =   'http://sourceforge.net/projects/hadoop-bam/files/latest/download'
-default['hadoop-BAM-repo']   =   'git://git.code.sf.net/p/hadoop-bam/code'
-default['hadoop-BAM-version'] = "5.1"
-default['tmp']                      =   '/tmp/hadoop-BAM'
-default['install_dir']              =   '/opt'
+default['hadoop-BAM-release-jar'] =   'http://sourceforge.net/projects/hadoop-bam/files/latest/download'
+default['hadoop-BAM-repo']        =   'git://git.code.sf.net/p/hadoop-bam/code'
+default['hadoop-BAM-version']     =   '5.1'
+default['tmp']                    =   '/tmp/hadoop-BAM'
+default['install_dir']            =   '/opt'
